66e08621f01b459ea8abe121539fb7a5d46b785b
a-camarillo <anthony.camarillo.96@gmail.com>
2021-04-11 21:13:19 -0700

    added min and max for weight and rep inputs

--- src/components/Set.vue
+++ src/components/Set.vue
@@ -1,10 +1,10 @@
 <template>
-    <div id="set" v-for="(set, setCounter) in sets" v-bind:key="setCounter">
+    <div id="set" v-for="(set, setCounter) in sets" :key="setCounter">
         <p>Set {{ setCounter+1 }}</p>
         <label>Weight:</label>
-        <input type="number" id="weight" v-model="set.weight">
+        <input type="number" id="weight" v-model="set.weight" min=0 max=1000>
         <label>Reps:</label>
-        <input type="number" id="reps" v-model="set.reps">
+        <input type="number" id="reps" v-model="set.reps" min=0 max=100>
     </div>
     <button @click="addSet">Add Set</button>
     <button @click="removeSet">Remove Set</button>
@@ -13,16 +13,23 @@
 <script>
 
 export default {
-    data() {
-        return {
-            sets: [
-                {
-                    weight: 0,
-                    reps: 0
-                }
-            ]
+    props: {
+        sets: {
+            type: Array,
+            default: [],
+            required: false,
         }
     },
+    // data() {
+    //     return {
+    //         sets: [
+    //             {
+    //                 weight: 0,
+    //                 reps: 0
+    //             }
+    //         ]
+    //     }
+    // },
     methods: {
     addSet() {
         this.sets.push({
