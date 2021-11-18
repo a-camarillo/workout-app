23c3e2ba15d1e9c078c8d306886994846b0a1ad5
Anthony Camarillo <58638902+a-camarillo@users.noreply.github.com>
2021-11-18 09:57:10 -0800

    hopefully this works now

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -15,9 +16,9 @@ app.use('/api/workouts', workouts);
 
 if (process.env.NODE_ENV === 'production') {
 
-    app.use(express.static(__dirname + '../dist/'));
+    app.use(express.static(path.join(__dirname, '../dist/')));
 
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '../dist/index.html'));
+    app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
 }
 
 app.listen(port);
