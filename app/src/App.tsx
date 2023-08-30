40e5f35e38b80496d63732bb60141404cabff4ee
a-camarillo <anthony.camarillo.96@gmail.com>
2023-08-30 11:18:15 -0700

    add initial dark mode theme

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,19 +7,23 @@ import '@fontsource/roboto/700.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { ThemeProvider } from '@mui/material/styles';
 import Home from './views/Home';
 import Workouts from './views/Workouts';
 import NewWorkout from './views/NewWorkout';
+import { darkTheme } from './themes';
 
 const App = () => {
   return (
   	<BrowserRouter>
 		<LocalizationProvider dateAdapter={ AdapterDayjs }>
+		<ThemeProvider theme={darkTheme}>
 			<Routes>
 				<Route path='/' element={<Home/>}/>
 				<Route path='/workouts' element={<Workouts/>}/>
 				<Route path='/new-workout' element={<NewWorkout/>}/>
 			</Routes>
+		</ThemeProvider>
 		</LocalizationProvider>
   	</BrowserRouter>
 
